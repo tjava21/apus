99f3b929049f4eef70a0fb65d703ab7f64c4c33c
Rafael Melo Sabino <rafael.sabino@cwi.com.br>
2021-07-20 12:03:07 -0300

    Teste da service UpdateBasketShippingService

--- src/test/java/br/com/cwi/apus/service/basket/UpdateBasketShippingServiceTest.java
+++ src/test/java/br/com/cwi/apus/service/basket/UpdateBasketShippingServiceTest.java
@@ -1,2 +1,48 @@
-package br.com.cwi.apus.service.basket;public class UpdateBasketShippingServiceTest {
+package br.com.cwi.apus.service.basket;
+
+import br.com.cwi.apus.domain.Basket;
+import br.com.cwi.apus.repository.BasketRepository;
+import br.com.cwi.apus.web.request.basket.UpdateBasketShippingRequest;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import static br.com.cwi.apus.utils.DomainUtils.*;
+
+@RunWith(MockitoJUnitRunner.class)
+public class UpdateBasketShippingServiceTest {
+
+    @InjectMocks
+    private UpdateBasketShippingService tested;
+
+    @Mock
+    private FindBasketByIdService findBasketByIdService;
+
+    @Mock
+    private RefreshBasketService refreshBasketService;
+
+    @Mock
+    private BasketRepository repository;
+
+    @Test
+    public void shouldSetAddress() {
+
+        Basket basket1 = Basket.builder().id(1L).build();
+        UpdateBasketShippingRequest request = new UpdateBasketShippingRequest();
+
+        Mockito.when(findBasketByIdService.getDomain(toExternalId(basket1.getId()))).thenReturn(basket1);
+
+        request.setAddress("teste, 435");
+        request.setZip("123456789");
+
+        tested.execute(toExternalId(basket1.getId()),request);
+
+        Mockito.verify(findBasketByIdService).getDomain(toExternalId(basket1.getId()));
+        Mockito.verify(refreshBasketService).execute(basket1);
+        Mockito.verify(repository).save(basket1);
+
+    }
 }
