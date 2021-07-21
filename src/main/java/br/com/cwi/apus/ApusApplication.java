ae930a0e6cd0edbcdf90939b509c0843f3bc98f9
Leonardo Freitas Pereira <pereira.leonardo@cwi.com.br>
2021-07-21 16:07:33 -0300

    issue-5 Cria job que atualiza estoque e situação de pedido para pedidos criados resolved #5

--- src/main/java/br/com/cwi/apus/ApusApplication.java
+++ src/main/java/br/com/cwi/apus/ApusApplication.java
@@ -9,11 +9,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
 import java.math.BigDecimal;
 
+@EnableScheduling
 @SpringBootApplication
 public class ApusApplication {
 
