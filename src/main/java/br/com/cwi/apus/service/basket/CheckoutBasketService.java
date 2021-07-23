c6eb7c8345a1e90f0e3e1c7b23a74be2e87676d7
Rafael Melo Sabino <rafael.sabino@cwi.com.br>
2021-07-23 14:47:53 -0300

    remove arquivos de order closes #17

--- src/main/java/br/com/cwi/apus/service/basket/CheckoutBasketService.java
+++ src/main/java/br/com/cwi/apus/service/basket/CheckoutBasketService.java
@@ -1,12 +1,9 @@
 package br.com.cwi.apus.service.basket;
 
 import br.com.cwi.apus.domain.Basket;
-import br.com.cwi.apus.domain.order.Order;
 import br.com.cwi.apus.exception.IntegrationException;
 import br.com.cwi.apus.external.lyra.PaymentService;
-import br.com.cwi.apus.mapper.OrderMapper;
 import br.com.cwi.apus.repository.BasketRepository;
-import br.com.cwi.apus.service.order.CreateOrderService;
 import br.com.cwi.apus.web.response.order.OrderResponse;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -21,8 +18,6 @@ public class CheckoutBasketService {
     private FindBasketByIdService findBasketByIdService;
     private PaymentService paymentService;
     private BasketRepository repository;
-    private CreateOrderService createOrderService;
-    private OrderMapper orderMapper;
 
     @Transactional
     public OrderResponse execute(String id) {
@@ -38,8 +33,7 @@ public class CheckoutBasketService {
         basket.setPaymentId(paymentId);
         repository.save(basket);
 
-        Order order = createOrderService.execute(basket);
 
-        return orderMapper.toOrderResponse(order);
+        return null;
     }
 }
--- src/test/java/br/com/cwi/apus/service/basket/CheckoutBasketServiceTest.java
+++ src/test/java/br/com/cwi/apus/service/basket/CheckoutBasketServiceTest.java
@@ -1,12 +1,8 @@
 package br.com.cwi.apus.service.basket;
 
 import br.com.cwi.apus.domain.Basket;
-import br.com.cwi.apus.domain.order.Order;
-import br.com.cwi.apus.domain.order.OrderStatus;
 import br.com.cwi.apus.external.lyra.PaymentService;
-import br.com.cwi.apus.mapper.OrderMapper;
 import br.com.cwi.apus.repository.BasketRepository;
-import br.com.cwi.apus.service.order.CreateOrderService;
 import br.com.cwi.apus.web.response.order.OrderResponse;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
@@ -34,21 +30,13 @@ public class CheckoutBasketServiceTest {
     @Mock
     private BasketRepository repository;
 
-    @Mock
-    private CreateOrderService createOrderService;
-
-    @Mock
-    private OrderMapper mapper;
-
     @Test
     public void shouldCheckoutBasketService() {
         // #1 - ARRANGE
         Basket basket = Basket.builder().id(1L).card("1234 4321").total(BigDecimal.TEN).build();
-        Order order = Order.builder().id(UUID.randomUUID()).status(OrderStatus.CRIADO).build();
 
         Mockito.when(findBasketByIdService.getDomain(toExternalId(basket.getId()))).thenReturn(basket);
         Mockito.when(paymentService.inform(basket.getCard(), basket.getTotal())).thenReturn(UUID.randomUUID().toString());
-        Mockito.when(createOrderService.execute(basket)).thenReturn(order);
 
         // #2 - ACT
         OrderResponse result = tested.execute(toExternalId(basket.getId()));
@@ -57,7 +45,5 @@ public class CheckoutBasketServiceTest {
         Mockito.verify(findBasketByIdService).getDomain(toExternalId(basket.getId()));
         Mockito.verify(paymentService).inform(basket.getCard(), basket.getTotal());
         Mockito.verify(repository).save(basket);
-        Mockito.verify(createOrderService).execute(basket);
-        Mockito.verify(mapper).toOrderResponse(order);
     }
 }
